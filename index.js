92b6a8d25eef438a4d5ac00ab5aca00628a474f9
Daniel Teles <dan.ribeiro@hotmail.com>
2023-02-09 00:36:05 +0000

    Practicing JavaScript: for...of; push; indexOf.

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
-// Simulando uma análise de BPC - Direito Previdenciário
+// Using a for..of loop to iterate through an array.
+var items = ["Car", "Shoe", "Soap", "Fan"];
 
-const prompt = require("prompt-sync")();
-console.log("Olá. Vamos fazer a análise do pedido de BPC/LOAS.\n");
-var nome = prompt("Digite o seu nome completo: ")
-var renda = parseFloat(prompt("Informe a sua renda mensal: "));
-console.log("\nAgora, precisamos saber a sua condição física.\n")
-var condicaoFisica = prompt("Portador de Deficiência ou Idoso? - Informe 'D' ou 'I': ");
-var familiares = parseFloat(prompt("\nQuantas pessoas compõem o núcleo familiar? "));
-for (i = 1; i <= familiares; i++) {
-  console.log("\nAgora, vamos aferir a renda do núcleo familiar como um todo. Informe os ganhos do", i, "° familiar.");
-  var renda_membroFamilia = parseFloat(prompt("Renda: "));
-  var renda = renda + renda_membroFamilia;
+for (let item of items) {
+  console.log(`A ${item}.\n`);
 }
-var renda_perCapita = renda / (familiares + 1);
-if ((condicaoFisica == "D" && renda_perCapita < 303) || (condicaoFisica == "I" && renda_perCapita < 303)) {
-  console.log("Informamos que", nome, "preenche os requisitos para solicitar o BPC.");
-  console.log("Renda per Capita R$: ", renda_perCapita.toFixed(2));
-} else {
-  console.log("Considerando os dados informados", nome, "não preenche os requisitos.");
-  console.log("Renda per Capita R$: ", renda_perCapita.toFixed(2));
+
+// Creating a function to show the index of each element.
+function showIndex(array) {
+  let result = [];
+  for (let i of array) {
+    result.push(array.indexOf(i));
+  }
+  return result;
 }
+
+// Calling the function inside a `console.log` to display the
+// index of each element.
+console.log(showIndex(items));
+
+// Creating a function to show if the array stores an specific
+// element.
+
+function showIndexOfElement(array, string) {
+  let found = false;
+
+  for (let j = 0; j < array.length; j++) {
+    if (array[j] === string) {
+      return `\n${string} is found at index ${j}!`;
+      found = true;
+    }
+  }
+
+  if (!found) {
+    return `\n${string} is not in this array.`;
+  }
+}
+
+// Calling the function inside a `console.log` to test
+// if it works properly.
+console.log(showIndexOfElement(items, "Boat"));
 
 
 
