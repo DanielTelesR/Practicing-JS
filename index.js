4d4e5b4821335044fad8385d0bcf65bf134795b4
Daniel Teles <dan.ribeiro@hotmail.com>
2023-02-12 01:04:54 +0000

    Favorite Brand - for, functions, .push, .includes()

--- index.js
+++ index.js
@@ -1,45 +1,35 @@
-// Using a for..of loop to iterate through an array.
-var items = ["Car", "Shoe", "Soap", "Fan"];
+const prompt = require('prompt-sync')();
 
-for (let item of items) {
-  console.log(`A ${item}.\n`);
-}
+// I created variables to store information;
+const brands = ["Nike", "Adidas", "Puma", "Vans"];
+const disliked = [];
+const fav_Brand = prompt(`Please, indicate your favorite brand: `)
 
-// Creating a function to show the index of each element.
-function showIndex(array) {
-  let result = [];
-  for (let i of array) {
-    result.push(array.indexOf(i));
-  }
-  return result;
-}
-
-// Calling the function inside a `console.log` to display the
-// index of each element.
-console.log(showIndex(items));
-
-// Creating a function to show if the array stores an specific
-// element.
-
-function showIndexOfElement(array, string) {
-  let found = false;
-
-  for (let j = 0; j < array.length; j++) {
-    if (array[j] === string) {
-      return `\n${string} is found at index ${j}!`;
-      found = true;
+/*I constructed a function to verify the user's preferences and return the brands that are disliked.*/
+function dislike(array) {
+  for (let i = 0; i < array.length; i++) {
+    let answer = prompt(`\nDo you like ${array[i]}? (Y or N): `);
+    if (answer == 'N') {
+      console.log(`\nGot it. You don't like ${array[i]}.\n`);
+      disliked.push(array[i]);
+    } else {
+      console.log(`\nGlad to hear that!\n`);
     }
   }
-
-  if (!found) {
-    return `\n${string} is not in this array.`;
-  }
+  return (`You dont like these brands: ${disliked}\n`);
 }
 
-// Calling the function inside a `console.log` to test
-// if it works properly.
-console.log(showIndexOfElement(items, "Boat"));
+/*I created another function to determine whether the user's preferred brand is on the list of "disliked" brands. If it is, the function outputs that this does not make sense. Otherwise, it declares that everything is okay.*/
 
+function brand_FindIndex(array, variable) {
+  array.includes(variable) ? console.log(`${variable} has been found in the list!\n`) : `${variable} is not in the list\n`;
+  if (array.includes(variable)) {
+    return `This doesn't make any sense, since you said ${fav_Brand} is your favorite brand.`
+  }
+  return `All is right in the Universe.`
+}
 
-
+// Output the results to the user
+console.log(dislike(brands));
+console.log(brand_FindIndex(disliked, fav_Brand));
 
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
-Practicing JavaScript: for...of; push; indexOf.
+Practicing JavaScript: for, functions, .push, .includes().
+
+I created this program to determine the user's preferred brand and verify if the given information is accurate. The program starts by asking the user to indicate their preferred brand. Next, it presents a list of brands and asks the user if they like each one. Whenever the answer is "No," the corresponding brand is added to a "disliked brands" list.
+
+Finally, if the user's preferred brand is in that list, the program outputs "That doesn't make any sense," indicating that the initial information provided was not true.
